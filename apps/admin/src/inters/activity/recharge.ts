591b98b626b79055ddb8f7e1cc1a625e03c34039
我不知道 <liweihao030925@gmail.com>
2025-12-31 22:21:22 +0800

    chore: update type

--- apps/admin/src/inters/activity/recharge.ts
+++ apps/admin/src/inters/activity/recharge.ts
@@ -6,7 +6,7 @@ export const zActivyRecharge = z.object({
   paymentId: z.number().default(0),
   userId: z.number().default(0),
   credits: z.string().default(''),
-  nonce: z.string().default(""),
+  nonce: z.string().nullable().default(""),
   amount: z.string().default(''),
   paymentStatus: z.enum(PAYMENT_STATUS).default(PAYMENT_STATUS.UNPAID),
   voucherCode: z.string().default("").nullable(),
