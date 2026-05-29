3565b0a0e3b0e202603c10654eb3347ce28f9d17
我不知道 <liweihao030925@gmail.com>
2026-05-29 17:48:27 +0800

    chore: 2026/05/29

--- apps/client/src/components/BaseFooter.vue
+++ apps/client/src/components/BaseFooter.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 const { settings } = useSettingStore()
-const { t, locale } = useI18n()
+// const { t, locale } = useI18n()
 
-const displayTitle = computed(() => {
-  return locale.value === 'zh' ? settings.title : settings.titleEn ? settings.titleEn : settings.title
-})
+// const displayTitle = computed(() => {
+//   return locale.value === 'zh' ? settings.title : settings.titleEn ? settings.titleEn : settings.title
+// })
 </script>
 
 <template>
   <footer class="flex justify-center space-x-1 my-2 text-xs text-muted-foreground">
-    <span>
+    <!-- <span>
       © {{ new Date().getFullYear() }} {{ displayTitle }}
       <a href="http://lushensanhe.com" target="_blank" class="underline text-primary">{{ t('footer.msg2') }}</a>
       {{ t('footer.msg3') }}
     </span>
-    <span>·</span>
+    <span>·</span> -->
     <span>
       <a href="https://beian.miit.gov.cn/" target="_blank" class="underline">
         {{ settings.beian }}
