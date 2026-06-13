fa6a003fa76a5842c9bfdf59ae0df3886ae27f9d
我不知道 <liweihao030925@gmail.com>
2026-06-13 10:28:19 +0800

    refactor: update useLocalStore

--- apps/client/src/stores/local.ts
+++ apps/client/src/stores/local.ts
@@ -1,16 +1,32 @@
 import { localApi } from "@/api/local"
 import { defineStore } from "pinia"
 
-export const useLocalStore = defineStore('useLocalStore', {
-  state: () => ({
-    localData: {} as any
-  }),
+// export const useLocalStore = defineStore('useLocalStore', {
+//   state: () => ({
+//     localData: {} as any
+//   }),
 
-  actions: {
-    async getLocalData() {
-      const local = localStorage.getItem('locale')
-      const res = await localApi.getLocal(local? local : 'zh')
-      this.localData = res.data
-    }
+//   actions: {
+//     async getLocalData() {
+//       const local = localStorage.getItem('locale')
+//       const res = await localApi.getLocal(local? local : 'zh')
+//       this.localData = res.data
+//     }
+//   }
+// })
+
+export const useLocalStore = defineStore('locale', () => {
+  const localData = ref<Record<string, string>>({})
+
+  async function getLocalData() {
+    const local = localStorage.getItem('locale')
+    const { data } = await localApi.getLocal(local ? local : 'zh')
+    localData.value = data
+  }
+
+  return {
+    localData,
+    
+    getLocalData,
   }
 })
